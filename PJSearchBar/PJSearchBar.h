a9038957e8e926e3af76b4a68364bd1e7a6dc967
sunpeijie_3wchina <sunpeijie@3wchina.net>
2016-11-03 18:01:15 +0800

    上传搜索框代码

---
+++ PJSearchBar/PJSearchBar.h
@@ -0,0 +1,13 @@
+//
+//  PJSearchBar.h
+//  PJSearchBar
+//
+//  Created by 孙培杰 on 16/11/3.
+//  Copyright © 2016年 sunpeijie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PJSearchBar : UISearchBar
+
+@end
---
+++ PJSearchBar/PJSearchBar.m
@@ -0,0 +1,21 @@
+//
+//  PJSearchBar.m
+//  PJSearchBar
+//
+//  Created by 孙培杰 on 16/11/3.
+//  Copyright © 2016年 sunpeijie. All rights reserved.
+//
+
+#import "PJSearchBar.h"
+
+@implementation PJSearchBar
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
