1ada31de034d51e49270a04e4f33aa2d8f9ab8cd
sunpeijie_3wchina <sunpeijie@3wchina.net>
2016-11-03 11:25:41 +0800

    Initial Commit

---
+++ PJSearchBar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PJSearchBar
+//
+//  Created by 孙培杰 on 16/11/3.
+//  Copyright © 2016年 sunpeijie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PJSearchBar/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  PJSearchBar
+//
+//  Created by 孙培杰 on 16/11/3.
+//  Copyright © 2016年 sunpeijie. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
